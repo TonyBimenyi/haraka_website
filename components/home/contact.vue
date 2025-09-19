8b0b3dde432a2f4a21fc57ba7ff85953a4fb75c0
TonyBimenyi <tonyblaisb@gmail.com>
2025-09-20 01:04:22 +0800

    ndahejeje contact

---
+++ components/home/contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="contact-section">
+   <div class="header-section-contact">
+  <div class="tagline-wrapper">
+    <div class="tagline">WE ARE HARMONI</div>
+  </div>
+  <div class="gallery_title">
+    <div class="title_ga">
+      <h1 class="main-title">Keep in touch <span>with Haraka Burundi Company</span></h1>
+    </div>
+    </div>
+    </div>
+
+    <form class="contact-form">
+      <div class="form-row">
+        <input type="text" placeholder="Your name" required />
+        <input type="email" placeholder="Email address" required />
+      </div>
+      <div class="form-row">
+        <input type="text" placeholder="Country" />
+        <input type="tel" placeholder="Phone number" />
+      </div>
+      <div class="form-row full">
+        <textarea rows="6" placeholder="Message"></textarea>
+      </div>
+      <div class="form-footer">
+        <button type="submit" class="btn">Send Email</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script setup>
+import "@/assets/css/contact.css";
+</script>
---
+++ components/home/testimonials.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="testimonials">
+    <!-- Section Heading -->
+    <div class="testimonials-header">
+      <p class="subtitle">TESTIMONIALS</p>
+      <h2 class="title">What They Say About Us</h2>
+    </div>
+
+    <!-- Testimonial Content -->
+    <div class="testimonial-content">
+      <!-- Profile Image -->
+      
+
+      <span class="quote">“</span>
+      <div class="profile-pic">
+            <img src="https://randomuser.me/api/portraits/women/44.jpg" alt="Profile of Jenni Hernandez" />
+      </div>
+      <p class="message">
+        Bring people together, or turn your passion into a business. Harmoni gives
+        you everything you need to host your best event yet. Lorem ipsum diamet.
+        Bring people together, or turn your passion into a business. Harmoni gives
+        you everything you need to host your best event yet.
+      </p>
+      <div class="author">
+        <h4>Jenni Hernandez</h4>
+        <p>Graphic Designer</p>
+      </div>
+    </div>
+
+    <!-- Slider Dots -->
+    <div class="testimonial-dots">
+      <span class="dot active"></span>
+      <span class="dot"></span>
+      <span class="dot"></span>
+    </div>
+  </section>
+</template>
+
+
+<script setup>
+import "@/assets/css/testimonials.css";
+</script>
